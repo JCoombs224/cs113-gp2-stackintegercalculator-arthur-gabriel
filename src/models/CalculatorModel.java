369a9f923303f650f1c1d5f6118078cd68d74b89
gabeBactol <gabrielbactol109@gmail.com>
2018-11-01 14:27:17 -0700

    Code from the book
    This is the code from the book. It does not work right now. This is just code that we can use.

--- src/models/CalculatorModel.java
+++ src/models/CalculatorModel.java
@@ -1,5 +1,9 @@
 package models;
 
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 /**
  * CalculatorModel.java : Concrete class using the stack data structure to evaluate infix math expressions.
  *
@@ -10,8 +14,82 @@ package models;
  */
 public class CalculatorModel implements CalculatorInterface {
 
+    public Stack<Integer> list = new Stack<Integer>();
+    private static final String OPERATORS = "+-*/";
+
+    public static class SyntaxErrorException extends Exception
+    {
+        SyntaxErrorException(String message)
+        {
+            super(message);
+        }
+    }
+
+
+
     @Override
-    public String evaluate(String expression) {
+    public String evaluate(String expression){
+        String[] tokens = expression.split("\\s+");
+        try
+        {
+            for(String nextToken: tokens)
+            {
+                char firstChar = nextToken.charAt(0);
+
+                if(Character.isDigit(firstChar))
+                {
+                    int value = Integer.parseInt(nextToken);
+                    list.push(value);
+                }
+                else if(isOperator(firstChar))
+                {
+                    int result = evalOP(firstChar);
+                    list.push(result);
+                }
+                else
+                {
+                    throw new SyntaxErrorException("hello");
+                }
+            }
+            int answer = list.pop();
+            if(list.empty())
+            {
+                return String.valueOf(answer);
+            }
+            else
+            {
+                throw new SyntaxErrorException("hello");
+            }
+        }
+        catch(EmptyStackException e)
+        {
+            throw new SyntaxErrorException("hello");
+        }
+
+
         return "NaN";
+    }
+    public boolean isOperator(char c)
+    {
+        return OPERATORS.indexOf(c) != -1;
+    }
+    private int evalOP(char op)
+    {
+        int rhs = list.pop();
+        int lhs = list.pop();
+        int result = 0;
+
+        switch(op)
+        {
+            case '+': result = lhs + rhs;
+                break;
+            case '-': result = lhs - rhs;
+                break;
+            case '*': result = lhs * rhs;
+                break;
+            case '/': result = lhs / rhs;
+                break;
+        }
+        return result;
     }
 }
